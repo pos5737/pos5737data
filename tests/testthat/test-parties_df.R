33468c98dbd196ce38756f84cde5509575d403ca
Carlisle Rainey <carlislerainey@gmail.com>
2019-10-05 10:42:32 -0400

    Fix typo

--- tests/testthat/test-parties_df.R
+++ tests/testthat/test-parties_df.R
@@ -1,7 +1,7 @@
 test_that("parties dataset replicates Clark and Golder's (2006) results", {
   # load data
   data(parties_df)
-  # # regression model from their table 2, pooled analysis, whole sample, plus country and year
+  # # regression model from their table 2, pooled analysis, whole sample
   fit <- lm(enep ~ log(average_magnitude)*eneg + upper_tier*eneg + en_pres*proximity, data = parties_df)
   # check the intercept
   expect_equivalent(round(coef(fit)["(Intercept)"], 2), 2.81)
